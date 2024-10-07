8aaa83bccc3cff6ac2a0d25fc4f7d362fc9a0122
tiwarideepanshu <deepanshutiwarinews@gmail.com>
2024-10-07 15:45:23 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,7 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        # Map of Roman numerals to their integer values
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0  # To keep track of the previous value
+        
+        for char in reversed(s):
+            current_value = roman_map[char]
+            if current_value < prev_value:
+                total -= current_value  # Subtract if current value is less than the previous
+            else:
+                total += current_value  # Add otherwise
+            prev_value = current_value  # Update previous value
 
-
-
+        return total
