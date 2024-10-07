04e288f4056cb051aa50e6b301c74ad1c5fc7800
tiwarideepanshu <deepanshutiwarinews@gmail.com>
2024-10-07 15:43:15 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        # Mapping of closing brackets to opening brackets
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        # Stack to keep track of opening brackets
+        stack = []
 
+        for char in s:
+            if char in bracket_map:  # If it's a closing bracket
+                top_element = stack.pop() if stack else '#'  # Pop the top element if stack is not empty
+                if bracket_map[char] != top_element:  # Check if it matches the corresponding opening bracket
+                    return False
+            else:  # It's an opening bracket
+                stack.append(char)
 
-
-
-
-
-
-    
-
-
-
-  
-
+        return not stack  # If stack is empty, all brackets were matched
